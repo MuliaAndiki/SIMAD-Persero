1616ff513095393469ee3cb5efbddf8042a1dc6c
Mulia Andiki <muliaandiki@gmail.com>
2026-08-06 16:52:54 +0700

    feat: config db

--- be/prisma/client/index.ts
+++ be/prisma/client/index.ts
@@ -1,4 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { env } from '../../src/config/env.config';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: env.DATABASE_URL,
+    },
+  },
+});
+
 export default prisma;
