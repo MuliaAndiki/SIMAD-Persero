6a1d58166f61aee3fc560a5c0a214f5bbfcf2d60
Mulia Andiki <muliaandiki@gmail.com>
2026-09-15 09:06:41 +0700

    fixed:endpoint cron

--- be/src/routes/cronRoutes.ts
+++ be/src/routes/cronRoutes.ts
@@ -1,41 +1,56 @@
-import type { AppContext } from "@/contex";
-import cronController from "@/controllers/CronController";
-import Elysia from "elysia";
+import type { AppContext } from '@/contex';
+import cronController from '@/controllers/CronController';
+import Elysia from 'elysia';
 
 class CronRouter {
   public cronRouter;
 
   constructor() {
-    this.cronRouter = new Elysia({ prefix: "/cron" });
+    this.cronRouter = new Elysia({ prefix: '/cron' });
     this.routes();
   }
 
   private routes() {
     // Database warm-up / ping (wake up DB connection to avoid cold start)
-    this.cronRouter.post("/ping", async (c: AppContext) => {
+    this.cronRouter.get('/ping', async (c: AppContext) => {
+      return cronController.pingDatabase(c);
+    });
+    this.cronRouter.post('/ping', async (c: AppContext) => {
       return cronController.pingDatabase(c);
     });
 
-    this.cronRouter.post("/warmup", async (c: AppContext) => {
+    this.cronRouter.get('/warmup', async (c: AppContext) => {
+      return cronController.PingService(c);
+    });
+    this.cronRouter.post('/warmup', async (c: AppContext) => {
       return cronController.PingService(c);
     });
 
     // Existing internship auto-start
-    this.cronRouter.post("/internship", async (c: AppContext) => {
+    this.cronRouter.get('/internship', async (c: AppContext) => {
+      return cronController.autoStartInternships(c);
+    });
+    this.cronRouter.post('/internship', async (c: AppContext) => {
       return cronController.autoStartInternships(c);
     });
 
-    this.cronRouter.get("/cleanup/preview", async (c: AppContext) => {
+    this.cronRouter.get('/cleanup/preview', async (c: AppContext) => {
       return cronController.previewInactiveUsers(c);
     });
 
     // Delete inactive users (PERMANENT!)
-    this.cronRouter.post("/cleanup/users", async (c: AppContext) => {
+    this.cronRouter.get('/cleanup/users', async (c: AppContext) => {
+      return cronController.deleteInactiveUsers(c);
+    });
+    this.cronRouter.post('/cleanup/users', async (c: AppContext) => {
       return cronController.deleteInactiveUsers(c);
     });
 
     // Delete orphaned files
-    this.cronRouter.post("/cleanup/files", async (c: AppContext) => {
+    this.cronRouter.get('/cleanup/files', async (c: AppContext) => {
+      return cronController.deleteOrphanedFiles(c);
+    });
+    this.cronRouter.post('/cleanup/files', async (c: AppContext) => {
       return cronController.deleteOrphanedFiles(c);
     });
   }
